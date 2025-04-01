98e587a73a7b0b81ca117e8cc74ed8cd866de3b5
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2025-04-01 07:55:30 -0500

    Initial Commit

--- src/main/java/com/hema/Palindrome.java
+++ src/main/java/com/hema/Palindrome.java
@@ -1,4 +1,21 @@
 package com.hema;
 
 public class Palindrome {
+    public static void main(String[] args) {
+        String x = "aradhya brilliance center";
+        char y[] = x.toCharArray();
+        int size = y.length;
+        y[0] = (char) (y[0] - 32);
+        int i = 1;
+        while (i != size) {
+            if (y[i] == ' ') {
+                y[i + 1] = (char) (y[i + 1] - 32);
+            }
+            ++i;
+        }
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
 }
--- src/main/java/com/hema/StringReverse.java
+++ src/main/java/com/hema/StringReverse.java
@@ -1,4 +1,50 @@
 package com.hema;
 
+import java.util.Scanner;
+
 public class StringReverse {
+    public static void main(String[] args) {
+        String x="ABC Tech";
+        char y[]=x.toCharArray();
+        int size=y.length;
+        char a[]=new char[size];
+        char b[]=new char[size];
+       int i=0;
+       while(i!=size){
+           a[i]=y[i];
+          // System.out.print(a[i]);
+           a[size-1-i]=y[i];
+           ++i;
+        }
+       System.out.println(y);
+       System.out.println(a);
+       System.out.println(b);
+    }
 }
+
+
+
+//package com.hema;
+//
+//import java.util.Arrays; // Import Arrays class
+//
+//public class StringReverse {
+//    public static void main(String[] args) {
+//        String str = "ABC Tech";
+//        char[] originalArray = str.toCharArray();
+//        int size = originalArray.length;
+//        char[] reversedArray = new char[size];
+//
+//        // Reverse the string
+//        for (int i = 0; i < size; i++) {
+//            reversedArray[size - 1 - i] = originalArray[i];
+//        }
+//
+//        // Print original and reversed arrays
+//        System.out.println("Original String: " + str);
+//        System.out.println("Original Array: " + Arrays.toString(originalArray));
+//        System.out.println("Reversed Array: " + Arrays.toString(reversedArray));
+//        System.out.println("Reversed String: " + new String(reversedArray));
+//    }
+//}
+//
